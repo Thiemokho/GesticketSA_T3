61d379507a875dd1bd6f4ebfdd89029ca09e2e10
bicaise <dbzbicaise022@gmail.com>
2019-10-16 08:33:44 +0000

    us3-reload

---
+++ us3/login.php
@@ -0,0 +1,128 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>INSCRIPTION</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+  </head>
+  <body>
+  <div class="container-fluid h-100">
+    <div class="row h-100">
+        <div class="col-2 collapse d-md-flex px-1 pt-2 h-100 border border-secondary" id="sidebar">
+            <ul class="nav flex-column w-100">
+               
+                <li class="nav-item">
+                    <a class="nav-link collapsed text-primary border border-secondary" href="#submenu1" data-toggle="collapse" data-target="#submenu1">Apprenant</a>
+                  
+                    <div class="collapse" id="submenu1" aria-expanded="false">
+                        <ul class="flex-column pl-2 nav">
+                            <li class="nav-item"><a class="nav-link py-0 text-secondary " href="#">Nouveau Apprenant</a></li>
+                            <li class="nav-item"><a class="nav-link py-0 text-secondary " href="#">Liste Apprenants</a></li>
+                            <li class="nav-item">
+                                <div class="collapse small" id="submenu1sub1" aria-expanded="false">
+                                    
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+
+            <img src="img/sonatel.png" class="img-fluid fixed-bottom mb-3 ml-2" >
+        </div>
+        <div class="col pt-2" style='background:url("img/bg1.jpg")'>
+        <h2 class="text-center text-primary">INSCRIPTION</h2>
+
+        <!-- Default form register -->
+<form class="text-center border border-light p-5" action="#!">
+
+  
+
+    <div class="form-row mb-4">
+        <div class="col">
+            <!-- First name -->
+            <input type="text" id="defaultRegisterFormFirstName" class="form-control" placeholder="Nom">
+        </div>
+        <div class="col">
+            <!-- Last name -->
+            <input type="text" id="defaultRegisterFormLastName" class="form-control" placeholder="Prenom">
+        </div>
+    </div>
+
+    <!-- E-mail -->
+    <input type="text" id="defaultRegisterFormEmail" class="form-control mb-4" placeholder="Profil">
+
+    <!-- Password -->
+    <input type="text" id="defaultRegisterFormPassword" class="form-control" placeholder="Session" aria-describedby="defaultRegisterFormPasswordHelpBlock">
+    <small id="defaultRegisterFormPasswordHelpBlock" class="form-text text-muted mb-4">
+    </small>
+
+    <!-- Phone number -->
+    <input type="text" id="defaultRegisterPhonePassword" class="form-control" placeholder="Numéro de téléphone" aria-describedby="defaultRegisterFormPhoneHelpBlock">
+    <small id="defaultRegisterFormPhoneHelpBlock" class="form-text text-muted mb-4">    </small>
+
+    <!-- Newsletter -->
+    <div class="custom-control custom-checkbox">
+        <input type="checkbox" class="custom-control-input" id="defaultRegisterFormNewsletter">
+        <label class="custom-control-label" for="defaultRegisterFormNewsletter">Se souvenir de moi</label>
+    </div>
+
+    <!-- Sign up button -->
+    <button class="btn btn-info my-4 btn-block bg-primary" type="submit">VALIDER</button>
+
+  
+
+    <hr>
+
+ 
+
+</form>
+<!-- Default form register -->
+         
+               
+         
+       
+</div>
+<style type="text/css">
+body, html {
+    height: 100%;
+}
+
+#sidebar {
+    min-width: 130px;
+}
+.nav-link[data-toggle].collapsed:after {
+    content: "▾";
+}
+.nav-link[data-toggle]:not(.collapsed):after {
+    content: "▴";
+}
+
+.text-primary {
+    color: #2b9595 !important;
+}
+.text-secondary {
+    color: #ff730e !important;
+}
+
+.img-fluid {
+    max-width: 50%;
+    height: 13%;
+}
+
+
+</style>
+      
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
+
